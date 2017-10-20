8c02e7c4606728f45e189526500f6235f8d5a5c6
Lai <laialexanderholmsen@gmail.com>
2017-10-20 19:10:03 +0200

    Renamed some variables. Added pos_overlay(), which will return a dict containing coordinates corresponding to the four corners, makes it easier to choose which corner you want the watermark in.

--- watermarker.py
+++ watermarker.py
@@ -4,33 +4,42 @@ from PIL import Image
 
 color = input("Which color? ")
 if color == "white" or color == "black":
-    overlay_image = Image.open("logo_" + color + ".png")
+    overlay_img = Image.open("logo_" + color + ".png")
 elif color != "default":
     print("Not a valid color, will use default colors. Use 'black', 'white' or 'default'")
-    overlay_image = Image.open("logo.png")
+    overlay_img = Image.open("logo.png")
 else:
-    overlay_image = Image.open("logo.png")
-start_image = Image.open("image.jpg")
+    overlay_img = Image.open("logo.png")
+start_img = Image.open("image.jpg")
 
-def get_new_overlay_size(start, overlay):
+def new_overlay_size(start, overlay):
     overlay_new_width = int(start.size[0] / 5)
     factor = overlay_new_width / overlay.size[0]
     overlay_new_height = int(overlay.size[1] * factor)
     return (overlay_new_width, overlay_new_height)
 
-resized_overlay = overlay_image.resize(get_new_overlay_size(start_image, overlay_image), Image.ANTIALIAS)
+new_overlay = overlay_img.resize(new_overlay_size(start_img, overlay_img), Image.ANTIALIAS)
 
-new_image = start_image.copy()
+new_img = start_img.copy()
 
 def get_bottom_right_pos(start, overlay):
     overlay_x_pos = start.size[0] - overlay.size[0]
     overlay_y_pos = start.size[1] - overlay.size[1]
     return (overlay_x_pos, overlay_y_pos)
 
-new_image.paste(resized_overlay, get_bottom_right_pos(new_image, resized_overlay), resized_overlay)
+def pos_overlay(start, overlay):
+    position = {
+        'top left': (0, 0),
+        'top right': (start.size[0] - overlay.size[0], 0),
+        'bottom left': (0, start.size[1] - overlay.size[1]),
+        'bottom right': (start.size[0] - overlay.size[0], start.size[1] - overlay.size[1])
+    }
+    return position
 
-extension = start_image.filename.split(".")[-1]
+new_img.paste(new_overlay, pos_overlay(new_img, new_overlay)['bottom right'], new_overlay)
 
-new_image.save("image_new." + extension)
+extension = start_img.filename.split(".")[-1]
+
+new_img.save("image_new." + extension)
 # may also do this, might be better:
 # new_image.save("newimage." + start_image.format)
