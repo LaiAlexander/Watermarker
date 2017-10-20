cef902287d61171327ebf943e82d99f107835a2e
Lai <laialexanderholmsen@gmail.com>
2017-10-20 18:02:09 +0200

    User may now choose either black, white or default color for the watermark. Made two functions for better readability.

--- watermarker.py
+++ watermarker.py
@@ -2,27 +2,35 @@
 
 from PIL import Image
 
+color = input("Which color? ")
+if color == "white" or color == "black":
+    overlay_image = Image.open("logo_" + color + ".png")
+elif color != "default":
+    print("Not a valid color, will use default colors. Use 'black', 'white' or 'default'")
+    overlay_image = Image.open("logo.png")
+else:
+    overlay_image = Image.open("logo.png")
 start_image = Image.open("image.jpg")
-overlay_image = Image.open("logo.png")
 
-start_width, start_height = start_image.size
+def get_new_overlay_size(start, overlay):
+    overlay_new_width = int(start.size[0] / 5)
+    factor = overlay_new_width / overlay.size[0]
+    overlay_new_height = int(overlay.size[1] * factor)
+    return (overlay_new_width, overlay_new_height)
 
-overlay_width, overlay_height = overlay_image.size
-
-overlay_new_width = int(start_width / 5)
-factor = overlay_new_width / overlay_width
-overlay_new_height = int(overlay_height * factor)
-
-resized_overlay = overlay_image.resize((overlay_new_width, overlay_new_height), Image.ANTIALIAS)
+resized_overlay = overlay_image.resize(get_new_overlay_size(start_image, overlay_image), Image.ANTIALIAS)
 
 new_image = start_image.copy()
-# bottom right corner
-overlay_x_pos = start_width - overlay_new_width
-overlay_y_pos = start_height - overlay_new_height
-new_image.paste(resized_overlay, (overlay_x_pos, overlay_y_pos), resized_overlay)
+
+def get_bottom_right_pos(start, overlay):
+    overlay_x_pos = start.size[0] - overlay.size[0]
+    overlay_y_pos = start.size[1] - overlay.size[1]
+    return (overlay_x_pos, overlay_y_pos)
+
+new_image.paste(resized_overlay, get_bottom_right_pos(new_image, resized_overlay), resized_overlay)
 
 extension = start_image.filename.split(".")[-1]
 
-new_image.save("newimage." + extension)
+new_image.save("image_new." + extension)
 # may also do this, might be better:
 # new_image.save("newimage." + start_image.format)
