bb876873f90dd473ef13b9ae0e95d018052a4917
Lai <laialexanderholmsen@gmail.com>
2019-04-12 16:51:05 +0200

    Cleaned up code. Starting support for bulk watermarking.

--- watermarker.py
+++ watermarker.py
@@ -2,25 +2,11 @@
 
 from PIL import Image
 
-color = input("Which color? ")
-if color == "white" or color == "black":
-    overlay_img = Image.open("logo_" + color + ".png")
-elif color != "default":
-    print("Not a valid color, will use default colors. Use 'black', 'white' or 'default'")
-    overlay_img = Image.open("logo.png")
-else:
-    overlay_img = Image.open("logo.png")
-start_img = Image.open("image.jpg")
-
 def new_overlay_size(start, overlay):
     overlay_new_width = int(start.size[0] / 5)
     factor = overlay_new_width / overlay.size[0]
     overlay_new_height = int(overlay.size[1] * factor)
     return (overlay_new_width, overlay_new_height)
-
-new_overlay = overlay_img.resize(new_overlay_size(start_img, overlay_img), Image.ANTIALIAS)
-
-new_img = start_img.copy()
 
 def get_bottom_right_pos(start, overlay):
     overlay_x_pos = start.size[0] - overlay.size[0]
@@ -36,16 +22,43 @@ def pos_overlay(start, overlay):
     }
     return position
 
-pos_text = input("In which corner? ")
+def watermark(overlay_img, pos_text, single):
+    if single:
+        start_img = Image.open("image.jpg")
 
-position = pos_overlay(new_img, new_overlay)
+        new_overlay = overlay_img.resize(new_overlay_size(start_img, overlay_img), Image.ANTIALIAS)
 
-position = position.get(pos_text) or position['bottom right']
+        new_img = start_img.copy()
 
-new_img.paste(new_overlay, position, new_overlay)
+        position = pos_overlay(new_img, new_overlay)
 
-extension = start_img.filename.split(".")[-1]
+        position = position.get(pos_text) or position['bottom right']
 
-new_img.save("image_new." + extension)
-# may also do this, might be better:
-# new_image.save("newimage." + start_image.format)
+        new_img.paste(new_overlay, position, new_overlay)
+
+        extension = start_img.filename.split(".")[-1]
+
+        new_img.save("image_new." + extension)
+        return
+    elif not single:
+        return
+
+def run():
+    color = input("Which color? ")
+    if color == "white" or color == "black":
+        overlay_img = Image.open("logo_" + color + ".png")
+    elif color != "default":
+        print("Not a valid color, will use default colors. Use 'black', 'white' or 'default'")
+        overlay_img = Image.open("logo.png")
+    else:
+        overlay_img = Image.open("logo.png")
+    pos_text = input("In which corner? ")
+    amount = input("Single image or bulk? ")
+    if amount == "single":
+        watermark(overlay_img, pos_text, True)
+    elif amount == "bulk":
+        watermark(overlay_img, pos_text, False)
+    # may also do this, might be better:
+    # new_image.save("newimage." + start_image.format)
+
+run()
