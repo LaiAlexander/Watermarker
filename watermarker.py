b6837ccef2cea6be38523d4af2af0f399d18a1a3
Lai <laialexanderholmsen@gmail.com>
2019-04-13 20:40:07 +0200

    Will get error messages instead of crashes if "images" doesn't exist or if the images aren't actually images.

--- watermarker.py
+++ watermarker.py
@@ -1,5 +1,10 @@
 """
 A script for watermarking pictures in bulk.
+TODO: 
+    Support for choosing size/ratio?
+    Error message if logo.png doesn't exist
+    If logo doesn't exist in black/white, display error or use color
+        May convert logo to black/white
 """
 #! python3
 
@@ -21,14 +26,20 @@ def pos_overlay(start, overlay):
     }
     return position
 
-def watermark(overlay_img, pos_text):
-    path = "images"
+def watermark(overlay_img, pos_text, path):
     # complete_path = os.getcwd() + path
     os.chdir(path)
     save_path = "watermarked"
     for filename in os.listdir(os.getcwd()):
         if os.path.isfile(filename):
-            start_img = Image.open(filename)
+            try:
+                start_img = Image.open(filename)
+            except OSError:
+                # black magic from https://stackoverflow.com/a/39675059
+                os.system('') #enable VT100 Escape Sequence for WINDOWS 10
+                print("Could not open \033[31m" +  filename + "\033[0m...", flush=True)
+                print("Are you sure it is an image file?", flush=True)
+                continue
             new_overlay = overlay_img.resize(new_overlay_size(start_img, overlay_img),
                                              Image.ANTIALIAS)
 
@@ -55,6 +66,12 @@ def watermark(overlay_img, pos_text):
     return
 
 def run():
+    path = "images"
+    if not os.path.exists(path):
+        print("Folder named 'images' does not exist.")
+        print("Create folder and place your images to be watermarked within.")
+        print("Exiting script...")
+        return
     color = input("Which color? ")
     if color == "white" or color == "black":
         overlay_img = Image.open("logo_" + color + ".png")
@@ -64,7 +81,7 @@ def run():
     else:
         overlay_img = Image.open("logo.png")
     pos_text = input("In which corner? ")
-    watermark(overlay_img, pos_text)
+    watermark(overlay_img, pos_text, path)
     # may also do this, might be better:
     # new_image.save("newimage." + start_image.format)
 
